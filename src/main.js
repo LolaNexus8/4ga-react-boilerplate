02e35e5596ac46a1970e5003f441ea9afd53e5df
LolaNexus8 <44571152+LolaNexus8@users.noreply.github.com>
2020-10-29 10:48:33 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./style.css"
+
+import Home from "./components/home.js"
+
+const container = document.querySelector("#app");
+const component = <Home />;
+
+ReactDOM.render(component, container);
